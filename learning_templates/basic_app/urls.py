ed4d1629c79e92f1bbb2020fe8741b323465fc79
lajerwin08 <lajerwin.cuizon13@gmail.com>
2022-08-29 08:53:53 +0800

    'first'

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from basic_app import views
+
+#TEMPLATE TAGGING
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('other/',views.other,name='other'),
+    path('relative/',views.relative,name='relative'),
+]
